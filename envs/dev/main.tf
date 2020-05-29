55208d6189d48d9dc798f9926eba43f8cf7afa3f
slock <stefan_lock@mcafee.com>
2020-05-29 11:56:48 +0100

    Working ECS build currently

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -11,3 +11,10 @@ provider "aws" {
 module "network" {
   source = "../../modules/network"
 }
+
+## ECS configuration
+module "ecs" {
+  source = "../../modules/ecs"
+	pub_sub_1 = module.network.pub_sub_1
+	pub_sub_2 = module.network.pub_sub_2
+}
